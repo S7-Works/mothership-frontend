906872a13f841a8d981104f455ea06be417a5091
Saumya Garg <sgargconsulting@gmail.com>
2025-09-09 05:07:06 +0530

    feat: improve navbar and page centering

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,20 +4,26 @@ import { AppBar, Toolbar, Typography, Button, Link } from '@mui/material';
 import { useAuth } from '../context/AuthContext';
 
 function Navbar() {
-    const { logout } = useAuth();
+    const { token, logout } = useAuth();
 
     return (
         <AppBar position="static">
             <Toolbar>
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    <Link component={RouterLink} to="/dashboard" color="inherit" underline="none">
+                    <Link component={RouterLink} to="/" color="inherit" underline="none">
                         Mothership
                     </Link>
                 </Typography>
-                <Button color="inherit" component={RouterLink} to="/dashboard">Environments</Button>
-                <Button color="inherit" component={RouterLink} to="/deployments">Deployments</Button>
-                <Button color="inherit" component={RouterLink} to="/teams">Teams</Button>
-                <Button color="inherit" onClick={logout}>Logout</Button>
+                {token ? (
+                    <>
+                        <Button color="inherit" component={RouterLink} to="/dashboard">Dashboard</Button>
+                        <Button color="inherit" component={RouterLink} to="/deployments">Deployments</Button>
+                        <Button color="inherit" component={RouterLink} to="/teams">Teams</Button>
+                        <Button color="inherit" onClick={logout}>Logout</Button>
+                    </>
+                ) : (
+                    <Button color="inherit" component={RouterLink} to="/login">Login</Button>
+                )}
             </Toolbar>
         </AppBar>
     );
